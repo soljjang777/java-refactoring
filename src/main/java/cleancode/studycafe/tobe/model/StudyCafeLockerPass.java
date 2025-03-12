ff4abd48a4d553ba3f8e19ebdc47d5cb42202e67
soljjang777 <vivace8520vi@naver.com>
2025-03-12 12:09:05 +0900

    [refactor] tobe폴더 생성 후 폴더 하단에 파일 복사 붙여넣기 함

---
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafeLockerPass.java
@@ -0,0 +1,53 @@
+package cleancode.studycafe.tobe.model;
+
+public class StudyCafeLockerPass implements Pass {
+
+    private final StudyCafePassType passType;
+    private final int duration;
+    private final int price;
+
+    private StudyCafeLockerPass(StudyCafePassType passType, int duration, int price) {
+        this.passType = passType;
+        this.duration = duration;
+        this.price = price;
+    }
+
+    public static StudyCafeLockerPass of(StudyCafePassType passType, int duration, int price) {
+        return new StudyCafeLockerPass(passType, duration, price);
+    }
+
+    @Override
+    public StudyCafePassType getPassType() {
+        return passType;
+    }
+
+    @Override
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public double getDiscountRate() {
+        throw new UnsupportedOperationException("지원하지 않는 기능입니다.");
+    }
+
+    @Override
+    public String display() {
+        if (passType == StudyCafePassType.HOURLY) {
+            return String.format("%s시간권 - %d원", duration, price);
+        }
+        if (passType == StudyCafePassType.WEEKLY) {
+            return String.format("%s주권 - %d원", duration, price);
+        }
+        if (passType == StudyCafePassType.FIXED) {
+            return String.format("%s주권 - %d원", duration, price);
+        }
+        return "";
+    }
+
+}
